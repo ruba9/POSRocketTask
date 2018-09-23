3e28e51c2b24f8ce3c0ad74404a0a91628e42f89
Ruba Aburub <abualrubruba@gmail.com>
2018-09-23 22:40:42 +0300

    Add files via upload

---
+++ pos.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void readInputArray(int InArr[], int size) { 
+    for(int i=0; i < size; i++) {
+        cout<<"please enter a positive value"<<endl;
+        cin>>InArr[i]; 
+        if(InArr[i] < 0) {
+     cout<<"negative values are invalid"<<endl;
+        }    
+    }
+    
+}
+
+void GiveOutputArray(int InArr[], int OutArr[], int size) {
+    int sum =0;
+    for(int i=0; i< size; i++) {
+        for(int j=InArr[i]; j>=0 ; j--) {
+            if(j == 0) {
+                 sum+=j;
+                 OutArr[i]=sum;
+                 sum =0;
+            }
+      sum+=j;  } 
+    }
+}
+
+int main()
+{
+    int size =4;
+    int InputArray[size];
+    int OutputArray[size];
+    
+    readInputArray(InputArray,size);
+    GiveOutputArray(InputArray,OutputArray,size);
+    
+    for(int i = 0; i < size; i++) {
+        cout<<"[ "<<OutputArray[i]<<" ] ";
+    }
+    
+
+    return 0;
+}
